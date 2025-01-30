6d01d18ebc43e7b7a59da06aa0c6ef706d42b868
pritty2004 <preeti.sharmait2021@indoreinstitute.com>
2025-01-30 14:03:26 +0530

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method"))
 
 main()
 .then(() => {
